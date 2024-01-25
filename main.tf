ccdeede7d75d234543f7d305fd30089fae6e2d5b
Joshua Odeyemi <joshodeyemi20@gmail.com>
2024-01-25 14:46:46 +0000

    Initialized Terraform in the directory

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  credentials = "./Terraform/Keys/my-creds.json"
+  project     = "terraform-intro-412309"
+  region      = "us-central1"
+}
+
+
+resource "google_storage_bucket" "terra-intro-bucket" {
+  name          = "terra-intro-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
